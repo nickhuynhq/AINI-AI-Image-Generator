8734fdea8cd048d7b79afd8d6c68b1c6de6363ec
Nicholas Huynh <nickhuynhq@gmail.com>
2023-02-08 16:11:05 -0500

    Create Home and CreatePage components

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import logo from './assets/logo.svg'
+import { Home, CreatePosts } from './pages'
 
 const App = () => {
   return (
