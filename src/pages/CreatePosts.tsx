d9f2e881a6525d3f864a77349916f267eb95643f
Nicholas Huynh <nickhuynhq@gmail.com>
2023-02-09 14:04:12 -0500

    Add additional formfields

--- src/pages/CreatePosts.tsx
+++ src/pages/CreatePosts.tsx
@@ -10,6 +10,12 @@ const CreatePosts = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const handleSubmit = () => {};
+
+  const handleChange = (e: any) => {};
+
+  const handleSurpriseMe = () => {};
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -19,6 +25,29 @@ const CreatePosts = () => {
           share them with the community
         </p>
       </div>
+
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField
+            labelName="Your Name"
+            type="text"
+            name="name"
+            placeholder="Type your name here..."
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="ie. an armchair in the shape of an avocado..."
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
     </section>
   );
 };
