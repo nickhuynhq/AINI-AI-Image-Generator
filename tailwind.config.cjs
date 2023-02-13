a523b1b49542eac95d2329ac45219a125b7ceca6
Nicholas Huynh <nickhuynhq@gmail.com>
2023-02-12 19:47:04 -0500

    Format and style Card component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,19 +14,6 @@ module.exports = {
         card: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.2)',
         cardhover: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.4)',
       },
-
-      // that is animation class
-      animation: {
-        fade: 'fadeOut 5s ease-in-out',
-      },
-
-      // that is actual animation
-      keyframes: theme => ({
-        fadeOut: {
-          '0%': { backgroundColor: theme('colors.red.300') },
-          '100%': { backgroundColor: theme('colors.transparent') },
-        },
-      }),
     },
   },
   plugins: [],
