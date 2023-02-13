62ac863826607fa22807db10822359df2fa20504
Nicholas Huynh <nickhuynhq@gmail.com>
2023-02-12 19:18:20 -0500

    Create downloadImage function in utils

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
 import { Post } from '../utils/types'
+import download from '../assets/download.png';
+import {downloadImage} from "../utils/index"
 
 
 const Card = ({...post}: Post) => {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import { surpriseMePrompts } from "../constants/index";
+import FileSaver from 'file-saver'
 
 export function getRandomPrompt(prompt: string): string {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
@@ -8,4 +9,8 @@ export function getRandomPrompt(prompt: string): string {
   if (randomPrompt === prompt) return getRandomPrompt(prompt);
 
   return randomPrompt;
+}
+
+export async function downloadImage(_id:string, photo: string) {
+  FileSaver.saveAs(photo, `download-${_id}.jpg`)
 }
