9b0c9f853a67c72251519d3f3cb40f9b19b4b8e0
Nicholas Huynh <nickhuynhq@gmail.com>
2023-02-15 16:54:48 -0500

    Format and style Register page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 import Home from './Home'
 import CreatePosts from './CreatePosts'
 import PostDetails from './PostDetails'
+import Register from './Register'
 
-export {Home, CreatePosts, PostDetails}
+export {Home, CreatePosts, PostDetails, Register}
